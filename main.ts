4555cc611046ea2ae754f91e8eff13d289190ed8
aloprui26-06 <102860876+aloprui26-06@users.noreply.github.com>
2022-04-29 18:32:32 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+basic.showLeds(`
+    . . # . .
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    `)
+music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+basic.pause(2000)
+basic.clearScreen()
 basic.forever(function () {
     basic.showNumber(input.temperature())
     basic.clearScreen()
